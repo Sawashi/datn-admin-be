6936dd29d4a6a424287777de19a92ffd7f008397
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-01-31 12:30:29 +0700

    Fix tables

--- src/cuisines/cuisines.module.ts
+++ src/cuisines/cuisines.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CuisinesService } from './cuisines.service';
 import { CuisinesController } from './cuisines.controller';
+import { Cuisine } from './cuisine.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cuisine])],
   providers: [CuisinesService],
-  controllers: [CuisinesController]
+  controllers: [CuisinesController],
 })
 export class CuisinesModule {}
--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { Review } from './review.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review])],
   providers: [ReviewsService],
-  controllers: [ReviewsController]
+  controllers: [ReviewsController],
 })
 export class ReviewsModule {}
