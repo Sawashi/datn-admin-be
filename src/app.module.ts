799016b77bde02a51a3003ce181c8276af869883
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-05-13 11:57:16 +0700

    feat: setup auto migrate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RecipesModule } from './recipes/recipes.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CuisinesModule } from './cuisines/cuisines.module';
@@ -27,16 +27,19 @@ import { CollectionModule } from './collections/collection.module';
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        type: process.env.DB_TYPE as any, // Change the value of the type property to "mysql"
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        autoLoadEntities: true,
+        synchronize: true, // Keep synchronize to automatically update the database schema
+      }),
+      inject: [ConfigService],
     }),
     RecipesModule,
     CategoriesModule,
