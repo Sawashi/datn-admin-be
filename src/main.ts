130bb7f26c1177ebbc7d678d096e81d80226cbad
Tan Sang <btsang20@clc.fitus.edu.vn>
2024-08-06 20:13:45 +0700

    feat: public cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
